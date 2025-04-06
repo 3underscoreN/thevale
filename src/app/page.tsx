d5ea3814c22035dfc9280c42ef797cae538dc17d
Kelvin <thisisrandom838@gmail.com>
2025-04-06 12:19:39 +0800

    chore: remove unused imports eslint is annoying yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import './page.css'
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import Footer from "@/components/Footer";
 
 export default function Home() {
 
