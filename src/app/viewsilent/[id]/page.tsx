427677bdc54f0245bd546378a493ba6761acc7ae
Kelvin <thisisrandom838@gmail.com>
2025-04-30 13:45:08 +0800

    fix(ux): remove title in view replies

--- src/app/viewsilent/[id]/page.tsx
+++ src/app/viewsilent/[id]/page.tsx
@@ -18,9 +18,7 @@ export default async function Page(params: { params: Promise<{ id: string }> }) 
   return (
     <div className="bg-[url(/asset/background.jpg)] bg-fixed bg-cover bg-no-repeat bg-center" tabIndex={-1}>
       <div className="flex flex-col items-center justify-center px-4 py-16 backdrop-blur-md backdrop-brightness-50">
-        <h1 className="text-4xl font-bold mt-16 mb-8">谷聲回響</h1>
-        <p className="text-lg mb-8">讓彼此的回聲在此共鳴。</p>
-        <div className="my-8 w-full md:w-3/4">
+        <div className="mt-16 mb-8 w-full md:w-3/4">
           <Button variant="outline" asChild>
             <Link href="/viewsilent">
               <span className="text-md">
@@ -28,7 +26,7 @@ export default async function Page(params: { params: Promise<{ id: string }> }) 
               </span>
             </Link>
           </Button>
-          <Card className="my-16">
+          <Card className="my-8">
             <CardHeader>
               <CardTitle className="text-2xl font-bold mb-2">
                 須知事項
