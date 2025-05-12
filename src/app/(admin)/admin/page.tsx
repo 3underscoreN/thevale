85bb7bad96ff35beae81bd050a2f0c7e6d3e5497
Kelvin <thisisrandom838@gmail.com>
2025-05-12 19:32:07 +0800

    feat(admin): add select component for fetching post types in AdminPage

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -9,6 +9,7 @@ import { type Item } from "@/interfaces/item";
 import { useState, useEffect } from "react";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 type responseFormat = {
   success: boolean;
@@ -41,6 +42,15 @@ export default function AdminPage() {
         <h1 className="text-4xl font-bold mt-16 mb-8">管理員介面</h1>
         <p className="text-lg mb-8">這裡是管理員介面，您可以在這裡查看和批准留言。</p>
         <div className="my-8 w-full md:w-3/4">
+          <Select onValueChange={setType as (value: fetchType) => void} name="type" defaultValue="silent">
+            <SelectTrigger>
+              <SelectValue placeholder="選擇留言..." />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="silent">靜谷之聲</SelectItem>
+              <SelectItem value="starlight">星光之聲</SelectItem>
+            </SelectContent>
+          </Select>
           {loading ?
             <>
               <Skeleton className="h-64 w-full bg-card rounded-xl my-4" />
