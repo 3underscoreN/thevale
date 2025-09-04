aefd3308dc972f801f9c7de66936990012ed67ad
Kelvin <thisisrandom838@gmail.com>
2025-09-04 21:00:43 +0800

    fix: update error handling and improve warning messages in chatbot component

--- src/app/(view)/chatbot/page.tsx
+++ src/app/(view)/chatbot/page.tsx
@@ -25,13 +25,14 @@ export default function Chat() {
   const [macLike, setMacLike] = useState<boolean>(false);
   const [isFormSubmitError, setIsFormSubmitError] = useState<boolean>(false);
   const [input, setInput] = useState<string>('');
-  const { messages, sendMessage, status, error, clearError } = useChat({
+  const { messages, setMessages, sendMessage, status, error, clearError } = useChat({
     transport: new DefaultChatTransport({
       api: '/api/chatbot'
     }),
     onError: async (error) => {
       console.error('Error sending message:', error);
       await delay(3000);
+      setMessages(messages.slice(0, -1));
       clearError();
     }
   });
@@ -76,9 +77,9 @@ export default function Chat() {
           <CardTitle className="text-2xl font-bold mb-2">須知事項</CardTitle>
           <CardDescription className="text-md">
             <ol className="list-decimal list-inside space-y-4">
-              <li>山谷聽友不能取代專業心理醫生以及心理諮商。如果需要更深入的幫助，請尋求專業心理健康資源。</li>
-              <li>我們希望山谷聽友能夠幫助每個人，所以請避免惡意使用山谷聽友，包括但不限於提示詞注入、發送不恰當信息等。</li>
-              <li>在使用山谷聽友時，請避免分享過於敏感的資訊。請參考
+              <li>「山谷聽友」不能取代專業心理醫生以及心理諮商。如果需要更深入的幫助，請尋求專業心理健康資源。</li>
+              <li>我們希望「山谷聽友」能夠幫助每個人，所以請避免惡意使用「山谷聽友」，包括但不限於提示詞注入、發送不恰當信息等。</li>
+              <li>在使用「山谷聽友」時，請避免分享過於敏感的資訊。請參考
                 <Button variant="link" size="icon" className="text-blue-500 inline text-md" asChild>
                   <Link href="/privacy">
                     私隱聲明
@@ -87,7 +88,7 @@ export default function Chat() {
                 以瞭解我們如何收集、使用所提供的數據，以及你所擁有的權利。
               </li>
               <li>服務提供商會過濾有關自傷的內容。對此限制我們深感抱歉，並正在尋求改善方案。</li>
-              <li>山谷聽友可能因技術調整、伺服器等因素而暫停運作。我們無法保證山谷聽友隨時可用。</li>
+              <li>「山谷聽友」可能因技術調整、伺服器等因素而暫停運作。我們無法保證「山谷聽友」隨時可用。</li>
             </ol>
           </CardDescription>
         </CardHeader>
